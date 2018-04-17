299d6b7e51a0f2fac3237d7190910919fbe0d11e
Patricio Santos <alexspatrick@Patricios-MBP.attlocal.net>
2018-04-17 10:06:47 -0500

    add bottoms main page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,6 +7,7 @@ import Parallax from 'react-image-parallax2';
 import index from 'react-image';
 
 
+
   const Banner = () => (
 <div>
   <div id="index-banner" className="Parallax-container">
@@ -17,11 +18,31 @@ import index from 'react-image';
         <div className="row center">
           
         </div>
-        <div className="row center">
+        // search bottom to google
+        <div className="row left">
 
-          <a href="http://google.com/" id="download-button" className="btn-large waves-effect waves-light purple accent-4">Get Started!</a>
+          <a href="http://google.com/" id="download-button" className="btn-large waves-effect waves-light purple accent-4">Google</a>
               
         </div>
+        //frame work for clink in bottom function from html
+        <div className="row left">
+
+          <a href=" " id="download-button" className="btn-large waves-effect waves-light purple accent-4">Cover Letter</a>
+              
+        </div>
+         //frame work for clink in bottom function from html
+        <div className="row left">
+
+          <a href=" "id="download-button" className="btn-large waves-effect waves-light purple accent-4">Reference Letter</a>
+              
+        </div>
+        //frame work for click in bottom function from html
+        <div className="row centers">
+
+          <a href=" " id="download-button" className="btn-large waves-effect waves-light purple accent-4">Resume</a>
+              
+        </div>
+
             <div>
               <Parallax
                 src={require("../styles/pictures/computer.jpg")} alt="Unsplashed background img 1" />
